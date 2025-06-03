36d1d625a5824ee2bcaf0903b31e83aa11d60e9c
mingyan.liu <mingyan.liu@zhaopin.com.cn>
2025-06-03 14:03:04 +0800

    初始化项目

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'yuebao-v1';
+const CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/icon-192.png',
+    '/icon-512.png',
+    '/manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(CACHE_URLS);
+            })
+    );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 处理fetch请求
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+}); 
